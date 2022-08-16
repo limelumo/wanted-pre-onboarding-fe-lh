ee9de14e0cb00d7114c163f9227b3f68b10f5799
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-17 04:01:31 +0900

    fix: Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import GlobalStyles from './styles/globalStyles';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import AuthWrapper from './components/AuthWrapper';
+import Auth from './pages/Auth';
+import TodoLists from './pages/TodoLists';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
+
       <div className="App">
         <Routes>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/" element={<AuthWrapper />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<Auth />} />
+          <Route path="/todo" element={<TodoLists />} />
+          <Route path="/sign-up" element={<Auth />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
         </Routes>
       </div>
     </BrowserRouter>
