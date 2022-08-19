62ddd614a84b31d35a7feb15193f21835e70c398
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-19 19:13:53 +0900

    refactor: simplify files

--- src/components/AuthWrapper.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const AuthWrapper = () => {
-  const authenticated = !!localStorage.getItem('token');
-
-  return <Navigate to={authenticated ? '/todo' : '/sign-in'} replace />;
-};
-export default AuthWrapper;
