29d1a02eb9c8d3ac0d3a7af446e892d5657222cb
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-20 17:30:02 +0900

    fix: remove getToken in TodoContext

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -4,10 +4,6 @@ const Header = ({ text }) => {
   return <Title>{text}</Title>;
 };
 
-Header.defaultProps = {
-  type: 'default',
-};
-
 const Title = styled.h2`
   font-size: 2em;
   margin-bottom: 1em;
