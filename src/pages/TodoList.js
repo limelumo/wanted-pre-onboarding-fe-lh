496b96ddc319c3a8ac041e89623441bfb4c28c2b
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-20 17:31:49 +0900

    feat: add Header & SingOut logic in TodoList

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 
 import NewTodo from '../components/Todo/NewTodo';
 import Todo from '../components/Todo/Todo';
+import Header from '../components/UI/Header';
 import TodoContext from '../store/TodoContext';
 
 const TodoList = () => {
@@ -19,17 +20,33 @@ const TodoList = () => {
     else getTodoData(token);
   }, [navigate]);
 
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    navigate('/', { replace: true });
+  };
+
   return (
     <TodoWrapper>
+      <Header text={'Todo List'} />
+
       <NewTodo />
       <Todo />
+
+      <SignOut>
+        <button onClick={handleSignOut}>로그아웃</button>
+      </SignOut>
     </TodoWrapper>
   );
 };
 
 const TodoWrapper = styled.div`
-  width: 100%;
-  border: 1px solid blue;
+  min-width: 400px;
+`;
+
+const SignOut = styled.section`
+  margin-top: 5em;
+  padding-top: 3em;
+  text-align: center;
 `;
 
 export default TodoList;
