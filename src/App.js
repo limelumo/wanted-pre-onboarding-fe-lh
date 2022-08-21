da79d78aa69dfd621e3ad16d7be20339b5c53cab
Lihu Kim <lh.ines.kim@gmail.com>
2022-08-22 00:19:12 +0900

    feat: add Loading suspense

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import GlobalStyles from './styles/globalStyles';
 import Auth from './pages/Auth';
-import TodoList from './pages/TodoList';
-
 import { TodoContextProvider } from './store/TodoContext';
+
+const TodoList = React.lazy(() => import('./pages/TodoList'));
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
 
-      <TodoContextProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Auth />} />
-            <Route path="/todo" element={<TodoList />} />
-            <Route path="/sign-up" element={<Auth />} />
-            <Route path="*" element={<Navigate to={'/'} />} />
-          </Routes>
-        </div>
-      </TodoContextProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TodoContextProvider>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Auth />} />
+              <Route path="/todo" element={<TodoList />} />
+              <Route path="/sign-up" element={<Auth />} />
+              <Route path="*" element={<Navigate to={'/'} />} />
+            </Routes>
+          </div>
+        </TodoContextProvider>
+      </Suspense>
     </BrowserRouter>
   );
 }
